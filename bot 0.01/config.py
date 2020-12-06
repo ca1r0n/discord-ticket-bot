93c64165aa044199158d1ddcfad94261cc3094d4
Egor <59088961+ca1r0n@users.noreply.github.com>
2020-12-06 03:09:51 +0300

    Add files via upload

---
+++ bot 0.01/config.py
@@ -0,0 +1,21 @@
+settings = {
+    'token': TOKEN,
+    'bot': "Теx. Поддержка",
+    'id': ID,
+    'prefix': '!',
+    'role_with_permisions':['Admin','helper'],
+    'role_id_helper':role_id_helper,
+    #эмоджи
+    'emoji_id_creat':784789313161265152,
+    'emoji_id_creat_full_name':'<:start:784789313161265152>',
+    'emoji_id_delete':784710383188574219,
+    'emoji_id_delete_full_name':'<:TNT:784710383188574219>',
+    'emoji_id_agree':784788263322779658,
+    'emoji_id_agree_full_name':'<:agree:784788263322779658>',
+    'emoji_id_disagre':784794647204855839,
+    'emoji_id_disagree_full_name':'<:disagre:784794647204855839>',
+    #авто
+    'id_channel_log': 0000,
+    'id_message_with_reacton': 0000,
+    'category': 0000
+}
